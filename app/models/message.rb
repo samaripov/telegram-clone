7ed9746d61085c2025744946a95088778753efd4
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-10-07 18:04:55 -0400

    Made chats have a unique id to receive turbo streams meant for them. And not receive any streams unrelated to that specific chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,11 @@ class Message < ApplicationRecord
   belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
 
   validates :text, length: { minimum: 1 }
-  broadcasts_to ->(message) { [ "messages" ] }, inserts_by: :append
+  
+  def chat_id
+    user_ids = [ sender_id, receiver_id ]
+    "#{user_ids.min}_#{user_ids.max}"
+  end
+
+  broadcasts_to ->(message) { [ message.chat_id, "messages" ] }, inserts_by: :append
 end
