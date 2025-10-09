105201c80bf9722467d4169bb16fb2b9b67ead5e
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-10-09 15:57:41 -0400

    Replaced the chat label with the name of the receiver in that chat.

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,4 +2,15 @@ class Chat < ApplicationRecord
   has_many :userchats
   has_many :users, through: :userchats
   has_many :messages
+
+  def label(current_user)
+    return nil unless self.persisted?
+    users_in_chat = self.users - [ current_user ]
+
+    if users_in_chat.length == 1
+      users_in_chat[0].username
+    else
+      self.name
+    end
+  end
 end
