c32c1693262d88912294f9d61d888f83b4c668fc
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-10-10 19:40:14 -0400

    Designed speach bubbles.

--- app/javascript/controllers/scroll_down_when_new_message_controller.js
+++ app/javascript/controllers/scroll_down_when_new_message_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="scroll-down-when-new-message"
 export default class extends Controller {
   connect() {
-    console.log(this.element.scrollHeight);
     this.resizeObserver = new ResizeObserver(this.scrollToTheBottom.bind(this));
     this.resizeObserver.observe(this.element);
   }
